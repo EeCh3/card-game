de70ad7552d0898dbd7823c2406cc598fd1afa38
EeCh3 <dirgeinferno42@gmail.com>
2023-04-05 18:46:24 +0300

    added TS file

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,52 @@
-const path = require("path");
+const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.ts",
-  mode: process.env.NODE_ENV === "production" ? "production" : "development",
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
+    entry: './src/index.ts',
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    optimization: {
+        minimizer: ['...', new CssMinimizerPlugin()],
+    },
+    devtool: process.env.NODE_ENV === 'production' ? false : 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'card-game.html',
+            template: './card-game.html',
+        }),
+        new CopyPlugin({
+            patterns: [{ from: 'src/pictures', to: 'static' }],
+        }),
     ],
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  optimization: {
-    minimizer: ["...", new CssMinimizerPlugin()],
-  },
-  devtool: process.env.NODE_ENV === "production" ? false : "source-map",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin({
-    filename: 'card-game.html',
-    template: './card-game.html',
-  }),
-  new CopyPlugin({
-    patterns: [{ from: 'scr/pictures', to: 'static' }],
-  })
-]
 };
